7ac69c185177c864c22e029fbfe06987451c1fca
binary_ho <dfghcvb11@naver.com>
2024-06-27 16:16:57 +0900

    hello! this is just a suggestion.

---
+++ java11/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+
+    String test = """
+        심웅냘훈러
+        """;
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
